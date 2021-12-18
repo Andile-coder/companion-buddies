ea4487fa65066d4955ff4a5ddbdd86a1cab29aa5
Andile-coder <maselaandile@gmail.com>
2021-12-18 19:45:13 +0200

    video and mission

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,7 @@ import "react-slideshow-image/dist/styles.css";
 import image1 from "../frailCare1.jpg";
 import image2 from "../frailCare2.jpg";
 import image3 from "../frailCare3.jpg";
+import health from "../health.png";
 function Home() {
   const slideImages = [
     {
@@ -47,20 +48,71 @@ function Home() {
         </div>
         <div className="home_container_content">
           <div className="home_container_content_left">
-            <iframe
-              height="280"
-              width="480"
-              src="https://www.youtube.com/embed/rokGy0huYEA"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
+            <div className="home_container_content_left_vid">
+              <iframe
+                height="280"
+                width="480"
+                src="https://www.youtube.com/embed/rokGy0huYEA"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+            <div className="home_container_content_left_services">
+              {/* <h1>Our Quality home care services,from R54/hr</h1>
+              <ul>
+                <li>
+                  <img src={health} />
+                  <p>service 1</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 2</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 3</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 4</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 5</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 6</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 7</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 8</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 9</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 10</p>
+                </li>
+                <li>
+                  <img src={health} />
+                  <p>service 11</p>
+                </li>
+              </ul> */}
+            </div>
           </div>
           <div className="home_container_content_right">
             <div className="home_container_content_right-box">
               <div>
-                <h3>Our Quality Care, Your Home</h3>
+                {/* <h1>Our Quality Care, Your Home</h1>
                 <p>
                   The word “home” is synonymous with love and feeling safe and
                   comfortable. It makes sense, then, that when we’re homesick,
@@ -75,20 +127,26 @@ function Home() {
                   provided nationwide by our compassionate, empathetic, and
                   certified caregivers, registered nurses, and companions (aka
                   Champs).
-                </p>
+                </p> */}
               </div>
               <div>
-                <h3>Companion Buddies Mission</h3>
+                <h1>Our Mission</h1>
                 <p>
-                  Our vision is to set the highest standard of home care in
-                  South Africa. It made sense, then, for us to partner with Nido
-                  Nursing—the academy recognised in the industry for the best
-                  quality healthcare assistant training.{" "}
+                  We at Companion Buddies we create relationships with our
+                  clients. We give them a sense of purpose. Treat them as if
+                  they were our family. The elderly has a wealth of knowledge
+                  that they love to share about their life and their experience
+                  which they sometimes don’t get to share. Here at Companion
+                  Buddies We like working with the seniors because we like
+                  helping them to become more independent and , helping them
+                  make a positive difference to their day and the most of their
+                  life that is left on earth be exciting, happy and the best it
+                  can be by providing these service
                 </p>
               </div>
             </div>
             <div className="home_container_content_right-box">
-              <h3>FROM CURE TO CARE</h3>
+              {/* <h1>FROM CURE TO CARE</h1>
               <p>
                 Every care plan is unique and we always tailor your plan to suit
                 your or your loved one’s needs.{" "}
@@ -105,13 +163,13 @@ function Home() {
                 recurring reservations, as well as on either an hourly or daily
                 basis. Expect weekly care reports and the option to rate your
                 Champ. Plus, we take care of all the admin.
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
         <hr />
         <div className="home_container_hours">
-          <p>
+          {/* <p>
             24/7 quality care provided nationwide Our regular office hours are
             Monday to Friday, from 7am - 4:30pm. National tel: +27 (0)87 944
             6299 Emergency number (weekends and public holidays): +27 (0)87 944
@@ -123,7 +181,7 @@ function Home() {
           </p>
           <p>
             We deliver quality care to your doorstep, starting from R54/hour.
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
